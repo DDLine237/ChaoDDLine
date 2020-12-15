5d2afbc7359c8dac1339f23dbfa86622f9b97c12
DianeZhu@237 <hongchauly231@gmail.com>
2020-12-15 21:12:25 +0700

    add

--- js/index.js
+++ js/index.js
@@ -3,6 +3,7 @@ $(document).on("DOMContentLoaded", DOMLoaded);
 $(document).on("submit", "#frm-login", Login);
 $(document).on("submit", "#frm-register", Register);
 
+
 function DOMLoaded() {
     $("#navbar").load("../html/menu.html");
 }
@@ -36,11 +37,9 @@ function Login(e)
                 else
                 {
                     alert("Login unsuccessfully!");
-                }
-            
+                }           
             }
-        });
-    
+        });    
 }
 
 
@@ -71,27 +70,63 @@ function Register(e)
     }
 }
 
-/*$("#product-all").ready(showAllProduct);
+$("#showAllProduct").ready(showProduct);
 
-function showAllProduct(){
-    $.ajax ({
-            type: "POST",
-            url: "../php/product_all.php",
-            success: function( result ) {
-                result = $.parseJSON(result);
-                displayProduct(result);
-                }
-            };)
+function showProduct(){
+    $("#showAllProduct").empty();
+    var products=
+    [
+        {id:1, name:"helo", price:"$20", img:"../Pic/heo.jpg"},
+        {id:2, name:"helo", price:"$20", img:"../Pic/heo.jpg"},
+        {id:3, name:"helo", price:"$20", img:"../Pic/heo.jpg"},
+        {id:3, name:"helo", price:"$20", img:"../Pic/heo.jpg"},
+        {id:4, name:"helo", price:"$20", img:"../Pic/heo.jpg"}
+    ];
+
+    
+    for(item of products){   
+        var text = `<div class="col-4 sm">
+                        <div class="card" id="heo">
+                            <div class="img-manual setBorder">
+                                <img src="${item.img}" class="img-fluid" alt="Responsive image">
+                            </div>
+                            <div class="card-body">
+                                <h5 class="card-title">${item.name}</h5>
+                                <p class="card-text"></p>
+                            </div>
+                             <div class="card-footer">
+                                <small>${item.price}</small>
+                            </div>
+                        </div>
+                    </div>`;
+        
+        $("#showAllProduct").append(text);
+    }
 }
 
-function displayProduct(products){
-$("#product-list").empty();
+function showProduct_php(){
+    $.ajax({
+        type: "POST", url: "../php/product.php",
+        success: function(result){
+            result = $.parseJSON(result);
+            if(result){
+                showProduct(result);
+            }
+            else{
+                return;
+            }
+        }
+    });
 
-    var product=[{
 
-    }];
-for(item of products){
-    var text='<div class="col-sm">
-              <div class="card" style="width:18rem;"
-                <img src="$(product[i].img}" class'
+/*<a data-product-id='${item.id}' 
+href*/
+
+
+/*$(document).on("click","#btn-view-details", Viewdetails);
+functions ViewDetails(product){
+    var id=$(this).data("product-id");
+    alert(id);
 }*/
+
+}
