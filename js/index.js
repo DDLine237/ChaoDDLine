e31f9ae1d6550f6f9efc4936ff249ca629e4168e
DianeZhu@237 <hongchauly231@gmail.com>
2020-12-23 20:53:38 +0700

    change container to menu

--- js/index.js
+++ js/index.js
@@ -155,16 +155,18 @@ function ViewDetails() {
     success: function( result ) {
         result = $.parseJSON(result);
     
-    $(".navbar_product").css("margin-top","");
+    $("#carouselExampleIndicators").css("margin-top","0px");
+    $("#carouselExampleIndicators").css("border","none");
+    $("#carouselExampleIndicators").empty();
+
+    $(".navbar_product").css("margin-top","0px");
     $(".navbar_product").css("border","none");
     $(".navbar_product").empty();
     
-    $(".new-arrival").css("margin-top","");
-    $(".new-arrival").css("border","none");
-    $(".new-arrival").empty();
 
-    $(".new-arrival-product").css("margin]-top","");
-    $(".new-arrival-product").empty();
+    $(".new-arrival").css("border","none");
+    $(".new-arrival").css("margin-top","0px");
+    $(".new-arrival").empty();
 
     $("#showAllProduct").empty();
     $("#product-details").empty();
